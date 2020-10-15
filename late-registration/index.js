a1059e44f9e55e15f01ac118eff063a2c0d8dc6d
NguyenNguyen <nguyenbk92@gmail.com>
2020-10-15 02:59:40 +0000

    Using forecast in 1 hour instead of current weather

--- late-registration/index.js
+++ late-registration/index.js
@@ -12,8 +12,8 @@ const key = process.env.WEATHER_API_KEY
 
 
 const main = async () => {
-    let res = await getJSON('http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + key, '', {}, 5000)
-    if (res.data.weather[0].main == 'Rain') {
+    let res = await getJSON('http://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=' + key+'&exclude=minutely,daily', '', {}, 5000)
+    if (res.data.hourly[0].weather[0].main == 'Rain') {
         let msg = process.env.SLACK_BOTNAME + " em xin phép đến muộn 1h ạ :thunder_cloud_and_rain: "
         notifySlack(msg, process.env.SLACK_HOOK_KEY, process.env.SLACK_CHANNEL, process.env.SLACK_BOTNAME, process.env.SLACK_BOT_ICON)
 
