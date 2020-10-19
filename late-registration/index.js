da8c094690b1fbc614819bcb8b6fa5fb7c111e7c
NguyenNguyen <nguyenbk92@gmail.com>
2020-10-19 03:32:51 +0000

    forecast in next 2 hours

--- late-registration/index.js
+++ late-registration/index.js
@@ -13,7 +13,7 @@ const key = process.env.WEATHER_API_KEY
 
 const main = async () => {
     let res = await getJSON('http://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=' + key+'&exclude=minutely,daily', '', {}, 5000)
-    if (res.data.hourly[0].weather[0].main == 'Rain') {
+    if (res.data.hourly[2].weather[0].main == 'Rain') {
         let msg = " em xin phép đến muộn 1h ạ :thunder_cloud_and_rain: "
         notifySlack(msg, process.env.SLACK_HOOK_KEY, process.env.SLACK_CHANNEL, process.env.SLACK_BOTNAME, process.env.SLACK_BOT_ICON)
 
