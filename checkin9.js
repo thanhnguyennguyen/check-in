9f7745f5fd9150a991047f7cace3b03476954082
NguyenNguyen <nguyenbk92@gmail.com>
2021-09-02 04:27:53 +0000

    add bot for checkin-checkout remind

--- checkin9.js
+++ checkin9.js
@@ -4,5 +4,5 @@ const noti_bot = require('noti_bot')
 //const notifyTelegram = noti_bot.telegram
 const notifySlack = noti_bot.slack
 
-let msg = process.env.SLACK_BOTNAME + " checked in :+1:"
+let msg = `Good morning <!channel> ! It's check-in time`
 notifySlack(msg, process.env.SLACK_HOOK_KEY, process.env.SLACK_CHANNEL, process.env.SLACK_BOTNAME, process.env.SLACK_BOT_ICON)
---
+++ checkout18.js
@@ -0,0 +1,36 @@
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, './.env') })
+const noti_bot = require('noti_bot')
+//const notifyTelegram = noti_bot.telegram
+const notifySlack = noti_bot.slack
+const axios = require('axios').default
+
+let msg = {
+	"channel" : process.env.SLACK_CHANNEL,
+	"username":  process.env.SLACK_BOTNAME,
+	"blocks" : [{"type":"section","text":{"type":"mrkdwn","text":" <!channel> It's check-out time. Pls fill up <https://docs.google.com/forms/d/e/1FAIpQLSdBY1y5v1GB6VIrotj-L9GDnKEfCcYBl4-Z8xDJEfCNN7wDxw/viewform|End of Day report>"}}],
+	"icon_emoji": process.env.SLACK_BOT_ICON,
+	
+}
+let params = {
+	payload: JSON.stringify(msg)
+}
+const data = Object.keys(params)
+  .map((key, index) => `${key}=${encodeURIComponent(params[key])}`)
+  .join('&');
+
+const options = {
+  method: 'POST',
+  headers: { 'content-type': 'application/x-www-form-urlencoded' },
+  data,
+  url: `https://hooks.slack.com/services/${process.env.SLACK_HOOK_KEY}`,
+};
+const main = async () => {
+	try {
+		await axios(options)
+	} catch(err) {
+		console.error(err)
+	}
+}
+
+main()
