54eff42ca5c5441a9758137a685bbe84de65295a
NguyenNguyen <nguyenbk92@gmail.com>
2022-03-04 17:52:00 +0700

    add PREFIX

--- checkin9.js
+++ checkin9.js
@@ -4,5 +4,5 @@ const noti_bot = require('noti_bot')
 //const notifyTelegram = noti_bot.telegram
 const notifySlack = noti_bot.slack
 
-let msg = `Good morning <!channel> ! It's check-in time`
+let msg = `${process.env.PREFIX} Good morning ! It's check-in time`
 notifySlack(msg, process.env.SLACK_HOOK_KEY, process.env.SLACK_CHANNEL, process.env.SLACK_BOTNAME, process.env.SLACK_BOT_ICON)
